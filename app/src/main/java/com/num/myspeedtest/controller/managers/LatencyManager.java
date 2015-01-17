39da176173178b583bdad97f6cafbd993cb7050e
Andrew Kim <andrewkim823@gmail.com>
2015-01-16 19:48:25 -0500

    Create new structure

---
+++ app/src/main/java/com/num/myspeedtest/controller/managers/LatencyManager.java
@@ -0,0 +1,15 @@
+package com.num.myspeedtest.controller.managers;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class LatencyManager {
+
+    private class LatencyHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message message) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/num/myspeedtest/controller/tasks/LatencyTask.java
@@ -0,0 +1,8 @@
+package com.num.myspeedtest.controller.tasks;
+
+public class LatencyTask implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
